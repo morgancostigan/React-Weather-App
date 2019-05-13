070b0f95d05c619f110bd6e662fb519ecf2e6c97
morgancostigan <morgan.costigan@gmail.com>
2019-05-13 14:34:55 -0500

    creates getWeather function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Results from './components/Results';
 import Form from './components/Form';
 
 class App extends React.Component{
+  getWeather = async () => {
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=55413,us&appid=${OPEN_WEATHER_API_KEY}&units=metric`);
+    const data = await api_call.json();
+  }
+
   render() {
     return (
       <div>
