934ee0e1bbb182178d26ebd3950b6582b74f3d34
morgancostigan <morgan.costigan@gmail.com>
2019-05-15 15:35:59 -0500

    setState with results from API call, ans state now displaying on DOM

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ require('dotenv').config();
 // const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 const API_KEY = "6361f0d3d15bb93fea66cfbab757cc7b";
 class App extends React.Component{
+  state = {
+    city: undefined,
+    temp: undefined,
+    high: undefined,
+    low: undefined,
+    humidity: undefined,
+    windspeed: undefined,
+    description: undefined,
+    error: undefined
+  }
   
   getWeather = async (e) => {
     console.log(API_KEY);
@@ -18,6 +28,18 @@ class App extends React.Component{
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      city: data.name,
+      temp: data.main.temp,
+      high: data.main.temp_max,
+      low: data.main.temp_min,
+      humidity: data.main.humidity,
+      windspeed: data.wind.speed,
+      description: data.weather[0].main,
+      error: ""
+    })
+    console.log(this.state);
+    
     
   }
 
@@ -26,7 +48,16 @@ class App extends React.Component{
       <div>
         <Title />
         <Form getWeather={this.getWeather}/>
-        <Results />
+        <Results 
+          city={this.state.city}
+          temp={this.state.temp}
+          high={this.state.high}
+          low={this.state.low}
+          humidity={this.state.humidity}
+          windspeed={this.state.windspeed}
+          description={this.state.description}
+          error={this.state.error }
+        />
       </div>
     )
   }
--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,13 @@ class Results extends React.Component {
     render() {
         return (
             <div>
-
+                <h3>Location: {this.props.city}</h3>
+                <h3>Current Temperature: {this.props.temp}</h3>
+                <p>Current Conditions: {this.props.description}</p>
+                <p>Temperature Range: {this.props.low}-{this.props.high}</p>
+                <p>Humidity: {this.props.humidity}</p>
+                <p>Wind: {this.props.windspeed} mph</p>
+                <p>{this.props.error}</p>
             </div>
         )
     }
