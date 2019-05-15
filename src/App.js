94104b9693eab1141d19158763489902df8b4528
morgancostigan <morgan.costigan@gmail.com>
2019-05-15 15:47:25 -0500

    setState only works if zipcode is defined

--- src/App.js
+++ src/App.js
@@ -27,17 +27,19 @@ class App extends React.Component{
 
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      city: data.name,
-      temp: data.main.temp,
-      high: data.main.temp_max,
-      low: data.main.temp_min,
-      humidity: data.main.humidity,
-      windspeed: data.wind.speed,
-      description: data.weather[0].main,
-      error: ""
-    })
+    if (zipcode){
+      console.log(data);
+      this.setState({
+        city: data.name,
+        temp: data.main.temp,
+        high: data.main.temp_max,
+        low: data.main.temp_min,
+        humidity: data.main.humidity,
+        windspeed: data.wind.speed,
+        description: data.weather[0].main,
+        error: ""
+      })
+}
     console.log(this.state);
     
     
