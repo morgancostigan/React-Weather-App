17b0cbec67e19786739bc6bb26612188fc7ac196
morgancostigan <morgan.costigan@gmail.com>
2019-05-15 16:11:28 -0500

    made Title and Form stateless components, cleaning up the code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,13 @@
 import React from "react"
 
-class Form extends React.Component {
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.props.getWeather}>
-                    <input type="text" name="zipcode" placeholder="Zip Code"/>
-                    <button>Get Weather</button>
-                </form>
+const Form = props => (
+    <div>
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="zipcode" placeholder="Zip Code" />
+            <button>Get Weather</button>
+        </form>
 
-            </div>
-        )
-    }
-};
+    </div>
+)
 
 export default Form;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,10 @@
 import React from "react"
 
-class Title extends React.Component{
-    render() {
-        return(
-            <div>
-                <h1>Can I Camp?</h1>
-                <h3>Weather conditions app</h3>
-            </div>
-        )
-    }
-};
+const Title = () => (
+    <div>
+        <h1>Can I Camp?</h1>
+        <h3>Weather conditions app</h3>
+    </div>
+)
 
 export default Title;
