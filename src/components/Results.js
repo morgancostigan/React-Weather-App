b9c62619bfce9826e0385bf64af79304f2b80353
morgancostigan <morgan.costigan@gmail.com>
2019-05-15 15:44:34 -0500

    result parameters not displaying on DOM until API results

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,12 +4,12 @@ class Results extends React.Component {
     render() {
         return (
             <div>
-                <h3>Location: {this.props.city}</h3>
-                <h3>Current Temperature: {this.props.temp}</h3>
-                <p>Current Conditions: {this.props.description}</p>
-                <p>Temperature Range: {this.props.low}-{this.props.high}</p>
-                <p>Humidity: {this.props.humidity}</p>
-                <p>Wind: {this.props.windspeed} mph</p>
+                {this.props.city && <h3>Location: {this.props.city}</h3>}
+                {this.props.temp && <h3>Current Temperature: {this.props.temp}°F</h3>}
+                {this.props.description && <p>Current Conditions: {this.props.description}</p>}
+                {this.props.high && this.props.low && <p>Temperature Range: {this.props.low}-{this.props.high}°F</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}%</p>}
+                {this.props.windspeed && <p>Wind: {this.props.windspeed} mph</p>}
                 <p>{this.props.error}</p>
             </div>
         )
