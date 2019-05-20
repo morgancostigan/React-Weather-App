d056c678b71a0a044d9ce681f6a65da0649a27c1
morgancostigan <morgan.costigan@gmail.com>
2019-05-20 16:26:05 -0500

    styles title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const Title = () => (
     <div>
-        <h1>Can I Camp?</h1>
-        <h3>Weather conditions app</h3>
+        <h1 className="title-container__title">Can I Camp?</h1>
+        <h4 className="title-container__subtitle">Weather conditions app</h4>
     </div>
 )
 
